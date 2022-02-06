68cf0dfe5e0dc49659f188fb9da8366e513d0f78
MODoming <aireyluz.md@gmail.com>
2022-02-06 03:01:03 -0300

    Mejorando funciones y CSS

--- js/juego.js
+++ js/juego.js
@@ -20,26 +20,26 @@ function palo(ctx, xi, yi, xf, yf){
 function dibujarMunieco(vidas){
     if (vidas === 5) {
         ctx.beginPath();
-        ctx.arc(250, 230, 30, 0, 2* Math.PI);
+        ctx.arc(200, 130, 30, 0, 2* Math.PI);
         ctx.stroke();
     };    
-    if (vidas === 4) {palo(ctx, 250, 260, 250, 350)};
-    if (vidas === 3) {palo(ctx, 250, 350, 280, 420)};
-    if (vidas === 2) {palo(ctx, 250, 350, 220, 420)};
-    if (vidas === 1) {palo(ctx, 250, 280, 280, 300)};
-    if (vidas === 0) {palo(ctx, 250, 280, 220, 300)};
+    if (vidas === 4) {palo(ctx, 200, 160, 200, 250)};
+    if (vidas === 3) {palo(ctx, 200, 250, 230, 320)};
+    if (vidas === 2) {palo(ctx, 200, 250, 170, 320)};
+    if (vidas === 1) {palo(ctx, 200, 180, 230, 200)};
+    if (vidas === 0) {palo(ctx, 200, 180, 170, 200)};
 };
 function dibujarTorre(){
-    ctx.clearRect(50, 50 , 300, 550);
+    ctx.clearRect(0, 0 , 250, 500);
     ctx.beginPath();
-    ctx.moveTo(50, 550);
-    ctx.lineTo(150, 500);
-    ctx.lineTo(250, 550);
+    ctx.moveTo(0, 450);
+    ctx.lineTo(100, 400);
+    ctx.lineTo(200, 450);
     ctx.closePath();
     ctx.stroke();
-    palo(ctx, 150, 500, 150, 150);
-    palo(ctx, 150, 150, 250, 150);
-    palo(ctx, 250, 150, 250, 200);
+    palo(ctx, 100, 400, 100, 050);
+    palo(ctx, 100, 050, 200, 050);
+    palo(ctx, 200, 050, 200, 100);
 };
 
 const esLetra = (caracter) => {
@@ -48,6 +48,8 @@ const esLetra = (caracter) => {
 };
 
 botonInicio.addEventListener("click", function(event){    
+    document.getElementById("mensaje1").innerHTML = ""
+    document.getElementById("mensaje2").innerHTML = ""
     event.preventDefault();
     dibujarTorre()
     document.getElementById("error").innerHTML = "";
@@ -58,7 +60,7 @@ botonInicio.addEventListener("click", function(event){
     for (i=0;i<aleatoria.length;i++){
         correcta[i] = "_";
     };
-    document.getElementById("correcta").innerHTML = "Respuesta: " + correcta.join(" ");   
+    document.getElementById("correcta").innerHTML = correcta.join(" ");   
     
     window.addEventListener("keypress", (event) => {        
         var codigo = event.key.toLowerCase();
@@ -67,14 +69,24 @@ botonInicio.addEventListener("click", function(event){
                 for (let i=0; i< aleatoria.length;i++){                    
                     if (aleatoria[i] === codigo){
                         correcta[i] = codigo.toUpperCase();
-                    };               
+                    };      
                 };                
-                document.getElementById("correcta").innerHTML = "Respuesta: " + correcta.join(" ");
-            } else if (!error.includes(codigo)){
+                document.getElementById("correcta").innerHTML = correcta.join("  ");
+                if (aleatoria.join("").toLowerCase() === correcta.join("").toLowerCase()){
+                    document.getElementById("mensaje1").innerHTML = "GANASTE!! Muy bien!!!";
+                    document.getElementById("mensaje2").innerHTML = "La respuesta es " + aleatoria.join("");
+                    document.getElementById("inicio").onfocus();
+                };
+            } else if (!error.includes(codigo) && vidas > 0){
                 vidas -= 1;
                 error += " " + codigo;
                 document.getElementById("error").innerHTML = error.toUpperCase();
                 dibujarMunieco(vidas);
+                if ( vidas === 0 ) {
+                    document.getElementById("mensaje1").innerHTML = "PERDISTE!!";
+                    document.getElementById("mensaje2").innerHTML = "La respuesta correcta era " + aleatoria.join("");
+                    document.getElementById("inicio").onfocus();
+                };
             };
         } else { alert("Solo se aceptan letras!!")};
     });
